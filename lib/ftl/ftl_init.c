d24c343b55e21599dac1817d4b538e4caed50f08
hyungjin.im <hyungjin.im@eeum.io>
2024-02-27 13:26:48 -0800

    fixed GET LOG Chunk Info time out issues

--- lib/ftl/ftl_init.c
+++ lib/ftl/ftl_init.c
@@ -240,7 +240,9 @@ ftl_retrieve_punit_chunk_info(struct spdk_ftl_dev *dev, const struct ftl_punit *
 			      struct spdk_ocssd_chunk_information_entry *info)
 {
 	uint32_t i = 0;
-	unsigned int num_entries = FTL_BLOCK_SIZE / sizeof(*info);
+	// ### hjim: take all blocks's bitmap at once
+	//unsigned int num_entries = FTL_BLOCK_SIZE / sizeof(*info);
+	unsigned int num_entries = dev->geo.num_chk;
 	struct ftl_ppa chunk_ppa = punit->start_ppa;
 	char ppa_buf[128];
 
@@ -1179,6 +1181,8 @@ spdk_ftl_dev_init(const struct spdk_ftl_dev_init_opts *_opts, spdk_ftl_init_fn c
 			goto fail_async;
 		}
 	}
+
+	SPDK_NOTICELOG("### Successfully finished FTL dev initialization\n");
 
 	return 0;
 fail_sync:
